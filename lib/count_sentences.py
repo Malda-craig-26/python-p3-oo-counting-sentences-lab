6a6ab3ab2d80c92a3e4b5b93d587044ddd4a1139
kimberly malda <maldakimberly228@gmail.com>
2025-05-14 13:22:09 +0300

    oo-counting-sentences

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,32 @@
 #!/usr/bin/env python3
 
+import re
+
 class MyString:
-  pass
+  def __init__(self,value = ""):
+    if isinstance(value,str):
+      print("The value must be a string.")
+      self.value = value 
+    else:
+        self.value = ""
+
+  def is_sentence(self):
+     return self.value.endswith(".")  
+
+  def is_question(self): 
+     return self.value.endswith("?") 
+  
+  def is_exclamation(self):
+      return self.value.endswith("!")
+  
+  def count_sentences(self):
+     sentences = re.split(r'[.?!]+(?=\s|$)', self.value)
+     return len([s for s in sentences if s.strip()])
+  
+text = MyString("Hello! How are you? This is a test. Another sentence.")
+print("Is sentence?",text.is_sentence())
+print("Is question?",text.is_question()) 
+print("Is exclamation?",text.is_exclamation())
+print("Number of sentences:",text.count_sentences)
+
+      
